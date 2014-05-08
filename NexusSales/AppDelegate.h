ae17bb389f04ea85fe7af6713fda80cd4b066faa
Dean Becker <dean.becker@nexusinds.com>
2014-05-08 14:30:05 +0100

    Initial Commit

---
+++ NexusSales/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NexusSales
+//
+//  Created by Dean Becker on 08/05/2014.
+//  Copyright (c) 2014 Nexus Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NexusSalesTests/NexusSalesTests.m
@@ -0,0 +1,34 @@
+//
+//  NexusSalesTests.m
+//  NexusSalesTests
+//
+//  Created by Dean Becker on 08/05/2014.
+//  Copyright (c) 2014 Nexus Industries. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NexusSalesTests : XCTestCase
+
+@end
+
+@implementation NexusSalesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
